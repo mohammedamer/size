241735d457c3082dc516dba96452b09b301b8b93
Mohammed Amer <mohammed.amer@fujitsu.com>
2025-12-04 19:03:52 +0000

    size in bytes

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,29 @@ use regex::Regex;
 use std::error::Error;
 use walkdir::WalkDir;
 
-pub fn total_size(root: String, pattern: String) -> Result<(), Box<dyn Error>> {
+pub fn total_size(root: String, pattern: String) -> Result<u64, Box<dyn Error>> {
+    let mut total_size = 0;
     let re = Regex::new(&pattern)?;
 
     for entry in WalkDir::new(root).into_iter().filter_map(Result::ok) {
+        if !entry.file_type().is_file() {
+            continue;
+        }
+
         if let Some(filename) = entry.file_name().to_str() {
             if re.is_match(filename) {
                 if let Some(path) = entry.path().to_str() {
-                    println!("{path}");
+                    let size = entry.metadata()?.len();
+
+                    total_size += size;
+
+                    println!("path: {path}\nsize: {size} bytes");
                 }
             }
         }
     }
 
-    return Ok(());
+    println!("total size: {total_size} bytes");
+
+    return Ok(total_size);
 }
